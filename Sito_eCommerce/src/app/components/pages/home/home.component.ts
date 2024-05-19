5f6d01286037a71b95253e9d443f4b0dd905c6c1
RobertBauer01 <bauer.robert@iisgalvanimi.edu.it>
2024-05-19 21:49:42 +0000

    Commit 5

--- Sito_eCommerce/src/app/components/pages/home/home.component.ts
+++ Sito_eCommerce/src/app/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { BiciService } from 'src/app/services/bici.service';
 import { Bicicletta } from 'src/app/shared/models/Bicicletta';
 
@@ -7,14 +8,23 @@ import { Bicicletta } from 'src/app/shared/models/Bicicletta';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  biciclette: Bicicletta[] = []
 
-  constructor(private biciService: BiciService) {
-    
-  }
+export class HomeComponent implements OnInit {
+  biciclette: Bicicletta[] = []
   
-  ngOnInit(): void {
-    this.biciclette = this.biciService.getAll();
+    constructor(private biciService: BiciService, private activatedRoute: ActivatedRoute) {
+      activatedRoute.params.subscribe((params) => {
+        if (params.searchTerm) {
+          this.biciclette = this.biciService.getAllBiciBySearchTerm(params.searchTerm);
+          if (this.biciclette.length === 0) {
+            this.biciclette = this.biciService.getAll();
+          }
+        } else {
+          this.biciclette = this.biciService.getAll();
+        }
+      });
+    }
+  
+    ngOnInit(): void {
+    }
   }
-}
